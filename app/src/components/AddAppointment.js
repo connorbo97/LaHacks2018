012a56032a8b28e8526dea713f77f0757c4d9077
Bradley <bradleymont@gmail.com>
2018-03-31 21:58:03 -0700

    adding to database almost finalized

--- app/src/components/AddAppointment.js
+++ app/src/components/AddAppointment.js
@@ -9,13 +9,13 @@ class AddAppointment extends Component
     super(props);
     this.state = 
     {
-      kaiserNumber:"",
-      month:"",
-      day:"",
-      year:"",
-      hour:"",
-      halfHour:"",
-      numIntervals:0,
+      kaiserNumber:"1234567890",
+      month:"03",
+      day:"12",
+      year:"2018",
+      hour:"09",
+      halfHour:"00",
+      numIntervals:5,
     }
 
     this.onClicked = this.onClicked.bind(this)
@@ -36,14 +36,16 @@ class AddAppointment extends Component
 
     var json = snap.val()
 
-    for(var chair in json)
-    {
-        let temp=json[chair].timeSlots
+    for(var chair=0; chair < 23; chair++){
+        if(!json[chair]){
+          return chair
+        }
+        let temp = json[chair].timeSlots
         let n = this.state.numIntervals
-        for(var i=this.state.hour; i < this.state.hour + Math.ceiling(this.state.numIntervals/2); i++)
+        for(var i=this.state.hour; i < this.state.hour + Math.ceil(this.state.numIntervals/2); i++)
         {
-          let j = i.toString().padString(2,"0")
-            
+          let j = i.toString().padStart(2,"0")
+          console.log(temp[j])
           if (temp[j])
           {
             if (temp[j].a)
@@ -87,33 +89,77 @@ class AddAppointment extends Component
     return -1
   }
 
-  async addToDatabase()
+  async addToDatabase(chairAvail)
   {
-    var snap = await fire.database.ref(`/patients/${kaiserNumber}/appointments`).update({})
+
+    var {day, month, year, hour, halfHour, numIntervals, kaiserNumber} = this.state
+    //check if the date is already taken
 
     var date = new Date()
-    date.setDay(parseInt(day))
+    date.setDate(parseInt(day))
     date.setMonth(parseInt(month))
     date.setYear(parseInt(year))
     date.setHours(parseInt(hour))
-    date.setMinutes(hparseInt(halfHour))
-    var json = {}
-    json[date.getDate()] = {chair:chairAvail,numIntervals:numIntervals}
-    fire.database.ref(`/patients/${kaiserNumber}/appointments`).update(json)
+    date.setMinutes(parseInt(halfHour))
+    let json = {}
+    console.log(date)
+    console.log(this.state)
+    json[date.getTime()] = {chair:chairAvail,numIntervals:numIntervals}
 
+    //ADD TO PATIENTS
+    console.log(json)
+    await fire.database.ref(`/patients/${kaiserNumber}/appointments`).update(json)
+
+
+    //ADD TO DATES
+    let i =0;
+    while(numIntervals > 0){
+      let tempJSON = {}
+      if(numIntervals >= 2){
+        tempJSON = {
+          a:{
+            taken:kaiserNumber
+          },
+          b: {
+            taken:kaiserNumber
+          }
+        }
+        numIntervals -= 2
+      } else {
+        tempJSON = {
+          a:{
+            taken:kaiserNumber
+          },
+          b:{
+            taken:-1
+          }
+
+        }
+        numIntervals -= 1
+      }
+
+      let temp = parseInt(hour) + i
+      await fire.database.ref(`/dates/${year}/${month}/${day}/chairs/${chairAvail}/timeSlots/${temp.toString().padStart(2,"0")}`).update(tempJSON) 
+      i++
+
+    }
   }
 
 
-  onClicked()
+  async onClicked()
   {
     var {day, month, year, hour, halfHour, numIntervals} = this.state
     //check if the date is already taken
-    var chairAvail = isAvailable(this.state)
+    var chairAvail = await this.isAvailable(this.state)
+    console.log(chairAvail)
     if (chairAvail == -1)
     {
       alert("Error: No time available")
       return
     }
+    console.log(chairAvail)
+    this.addToDatabase(chairAvail)
+
 
     //add it to the dates branch
   }
@@ -126,7 +172,7 @@ class AddAppointment extends Component
         <input type="text" placeholder="MM" size="2" maxLength="2"/>/
         <input type="text" placeholder="DD" size="2" maxLength="2"/>/
         <input type="text" placeholder="YYYY" size="4" maxLength="4"/>
-        <button onClicked={this.onClicked}/>
+        <button onClick={this.onClicked}/>
       </div>
     );
   }
